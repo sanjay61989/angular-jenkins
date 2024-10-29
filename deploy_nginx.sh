823c2235dee41b9f03b888c36395d0a7192496d2
Sanjay Chhabria <sonu61989@gmail.com>
2024-10-29 17:23:13 +0530

    modified docker

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -1,6 +1,6 @@
 sudo rm -rf /usr/share/nginx/html/*
 sudo cp -r /tmp/build/* /usr/share/nginx/html
-sudo cp nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp /var/lib/jenkins/workspace/myapp/mfe-shell/nginx/nginx.conf /etc/nginx/nginx.conf
 sudo nginx -t
 sudo systemctl stop nginx
 sudo systemctl start nginx
